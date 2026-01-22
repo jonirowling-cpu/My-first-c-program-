fe2ade39c9553377556d35d4e8493cdbdd0874db
jonirowling-cpu <jonirowling@gmail.com>
2026-01-22 23:11:12 +0530

    Problem1.c

---
+++ si and ci.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<math.h>
+int main(){
+float p,r,t,si,ci;
+printf("enter principle rate and time");
+scanf("%f %f %f",&p,&r,&t);
+si=(p*r*t)/100;
+ci=p*(pow((1+r/100),t))-p;
+printf("si=%2f\n",si);
+printf("ci=%2f\n",ci);
+return 0;}
